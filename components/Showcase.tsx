ce395f553a6a5996cfd92e2048057e33719176d0
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-02-04 10:36:44 +0600

    showcase design done

--- components/Showcase.tsx
+++ components/Showcase.tsx
@@ -2,6 +2,7 @@ import styles from '@/styles/Showcase.module.css';
 function Showcase() {
   return (
     <div className={styles.showcase}>
+      <h1>Welcome to showcase</h1>
       <h2>I am showcase</h2>
     </div>
   );
