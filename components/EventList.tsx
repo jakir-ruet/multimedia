7a6f9d05ad28d45b39784a0fe024918a423035e9
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-02-08 16:51:56 +0600

    events page design done

--- components/EventList.tsx
+++ components/EventList.tsx
@@ -4,45 +4,41 @@ import styles from '@/styles/EventList.module.css';
 function EventList({ individualEvent }) {
   return (
     <>
-      <div className="container mx-auto max-w-7xl p-2 md:p-2 bg-gray-200">
-        <div className="columns-3 gap-2">
-          <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+      <div className="grid grid-cols-1 grid-rows-1 bg-gray-200 p-3 gap-2">
+        <div className="mt-3 mx-auto bg-white rounded-xl shadow-md overflow-hidden">
+          <div className="md:flex">
             <Image
-              className="w-24 h-24 md:w-480 md:h-auto md:rounded-none rounded-full mx-auto"
+              className="md:w-250 md:h-auto md:rounded-none rounded-full mx-auto"
               src={
                 individualEvent.image
                   ? individualEvent.image
                   : '/images/event-default.png'
               }
-              // src="/images/sample/event1.jpg"
               height={250}
               width={250}
               alt="event1"
             />
-            <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-              <blockquote>
-                <p className="text-lg font-medium">
-                  {individualEvent.description}
-                </p>
-              </blockquote>
-              <figcaption className="font-medium">
-                <div className="text-sky-500 dark:text-sky-400">
-                  {individualEvent.name}
-                </div>
-                <div className="text-slate-700 dark:text-slate-500">
-                  <h3>{individualEvent.venue}</h3>
-                </div>
-                <div className="text-slate-700 dark:text-slate-500">
-                  <h3>
-                    {individualEvent.date} at {individualEvent.time}
-                  </h3>
-                </div>
-                <div className="text-slate-700 dark:text-slate-500">
-                  <h3>{individualEvent.performers}</h3>
-                </div>
-              </figcaption>
+            <div className="p-8">
+              <p className="mt-2 text-slate-500">
+                {individualEvent.description}
+              </p>
+              <p>{individualEvent.name}</p>
+              <h3>{individualEvent.venue}</h3>
+              <p>
+                {individualEvent.date} at {individualEvent.time}
+              </p>
+              <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+                <h3>{individualEvent.performers}</h3>
+              </div>
             </div>
-          </figure>
+            <div className="p-8 inline-block align-middle">
+              <div className="p-8 inline-block align-middle">
+                <button className="bg-red-500 mt-4 p-3 rounded-xl">
+                  Details
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/pages/api/events/[slug].tsx
+++ src/pages/api/events/[slug].tsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { events } from '../data.json';
 export default (req, res) => {
+  //filter the data and checking from remotely
   const individualEvent = events.filter((ie) => ie.slug === req.query.slug);
   if (req.method === 'GET') {
     res.status(200).json(individualEvent);
